0b00447952a2c92a765565cd8a6c6327f621256e
svivek19 <svivek030503@gmail.com>
2024-03-23 18:10:25 +0530

    entries

--- arrayMethod.js
+++ arrayMethod.js
@@ -17,3 +17,28 @@ const number = [1, 2, 3, 4, 5];
 const result = number.map((val) => val * 2);
 
 // console.log(result);
+
+//at()
+
+const characters = ["a", "b", "c"];
+
+// console.log(characters[1]);
+// console.log(characters.at(-2));
+
+//copyWithin()
+
+const fruitss = ["apple", "banana", "orange"];
+
+// console.log(fruitss.copyWithin(0, 1, 2));
+
+// console.log(fruitss.copyWithin(1, 2));
+
+//entries()
+
+const tools = ["vscode", "pycompiler", "photoshop"];
+
+const toolsEntries = tools.entries();
+
+for (let [tools, index] of toolsEntries) {
+  //   console.log(tools, index);
+}
