5b258db1317b85ea1f475249e770a7afb4daeb9a
svivek19 <svivek030503@gmail.com>
2024-03-23 12:45:18 +0530

    event loops

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-// async & await
+//Event loops
 
-const loginForm = document.getElementById("loginForm");
-const response = document.getElementById("response");
+const welcome = () => console.log("hello");
+const load = () =>
+  setTimeout(() => {
+    console.log("load");
+  }, 2000);
+const one = () => console.log("one");
 
-let userData = null;
+welcome();
+load();
+one();
 
-const showResponse = (userData) => {
-  response.innerHTML = userData;
-};
-
-const verifyLogin = (name, pass) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (name == "vivek" && pass == "vivek") {
-        userData = "login success";
-        resolve(userData);
-      } else {
-        error = "incorrect details";
-        reject(error);
-      }
-    }, 2000);
-  });
-};
-
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const name = document.getElementById("name").value;
-  const pass = document.getElementById("pass").value;
-
-  async function login() {
-    try {
-      await verifyLogin(name, pass);
-      showResponse(userData);
-    } catch {
-      showResponse(error);
-    }
-  }
-  login();
-});
+// hello
+// one
+// load
