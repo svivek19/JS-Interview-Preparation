b74f8fccf9f6a185b9e5b562b25e37fc4265a8ca
svivek19 <svivek030503@gmail.com>
2024-03-23 17:57:34 +0530

    map

--- arrayMethod.js
+++ arrayMethod.js
@@ -1,7 +1,19 @@
-//forEach
+//forEach()
 
 const fruits = ["apple", "banana", "orange"];
 
 fruits.forEach((fruit) => {
   //   console.log(fruit.toLocaleUpperCase());
 });
+
+//map()
+
+const modifyData = fruits.map((fruit) => fruit.toUpperCase());
+
+// console.log(modifyData);
+
+const number = [1, 2, 3, 4, 5];
+
+const result = number.map((val) => val * 2);
+
+// console.log(result);
